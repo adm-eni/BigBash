d642e6fa631c148558e2269b17ae57ea765b8bed
Clement <clement.marcade2024@campus-eni.fr>
2024-06-25 19:16:40 +0200

    chore(Fixtures): création jeu de données users/campus

--- src/Controller/OutingController.php
+++ src/Controller/OutingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\OutingRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,9 +11,31 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('', name: 'outing_')]
 class OutingController extends AbstractController
 {
-    #[Route('', name: 'list')]
-    public function list(): Response
-    {
-        return $this->render('outing/outing.index.html.twig');
-    }
+  #[Route('', name: 'list')]
+  public function list(
+      OutingRepository $outingRepo
+  ): Response
+  {
+
+//    $outings = $outingRepo->findAll();
+
+    return $this->render('outing/outing.index.html.twig',
+//        [
+//        'outings' => $outings
+//    ]
+    );
+  }
+//
+//  #[Route('/outings/{id}', name: 'outing', requirements: ['id' => '\d+'])]
+//  public function outing(
+//      int              $id,
+//      OutingRepository $outingRepo
+//  ): Response
+//  {
+//    $outing = $outingRepo->find($id);
+//
+//    return $this->render('outing/outing.show.html.twig', [
+//        'outing' => $outing
+//    ]);
+//  }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,19 +2,54 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Campus;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private readonly Generator $faker;
+
+    public function __construct(private readonly UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->addCampus(1, $manager);
+        $this->addUsers(1, $manager);
+
+    }
+
+    public function addCampus(int $number, ObjectManager $manager): void {
+        $campus = new Campus();
+        $campus->setName("Rennes");
+        $manager->persist($campus);
+        $manager->flush();
+    }
+    public function addUsers(int $number, ObjectManager $manager): void {
+        $campus = $manager->getRepository(Campus::class)->findAll();
+        for ($i = 0; $i < $number; $i++) {
+            $user = new User();
+            $user->setEmail($this->faker->email());
+            $user->setLastName($this->faker->lastName());
+            $user->setFirstName($this->faker->firstName());
+            $user->setPhoneNumber($this->faker->phoneNumber());
+            $user->setRoles(['ROLE_USER']);
+            $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
+            $user->setActive(true);
+            $user->setCampus($campus[array_rand($campus)]);
+
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
-
 
 
 }
