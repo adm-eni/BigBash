0b1c81bf23a1b9d358496f79003ff87e0566e000
Adam <adam.siran2022@campus-eni.fr>
2024-06-25 14:54:32 +0200

    build(readme): suite config tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sunset: {
+          50: '#fff3f0',
+          100: '#ffe3db',
+          200: '#ffc7b8',
+          300: '#fca790',
+          400: '#f48465',
+          500: '#f16741',
+          600: '#de461b',
+          700: '#c8360e',
+          800: '#9e2605',
+          900: '#811f03',
+        },
+      },
+    },
   },
   plugins: [],
 }
