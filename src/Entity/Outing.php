ae250d3fdd97d9beeadfc40928504ee9540195dc
Adam <adam.siran2022@campus-eni.fr>
2024-06-28 17:59:37 +0200

    chore(outing): ajouter getter fin sortie

--- src/Entity/Outing.php
+++ src/Entity/Outing.php
@@ -11,210 +11,213 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: OutingRepository::class)]
 class Outing
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column]
+  private ?int $id = null;
 
-    #[ORM\Column(length: 255, nullable: false)]
-    private ?string $title = null;
+  #[ORM\Column(length: 255, nullable: false)]
+  private ?string $title = null;
 
-    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
-    private ?\DateTimeInterface $startAt = null;
+  #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+  private ?\DateTimeInterface $startAt = null;
 
-    #[ORM\Column(nullable: true)]
-    private ?int $duration = null;
+  #[ORM\Column(nullable: true)]
+  private ?int $duration = null;
 
-    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
-    private ?\DateTimeInterface $entryDeadline = null;
+  #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+  private ?\DateTimeInterface $entryDeadline = null;
 
-    #[ORM\Column(nullable: true)]
-    private ?int $maxEntryCount = null;
+  #[ORM\Column(nullable: true)]
+  private ?int $maxEntryCount = null;
 
-    #[ORM\Column(type: Types::TEXT, nullable: true)]
-    private ?string $description = null;
+  #[ORM\Column(type: Types::TEXT, nullable: true)]
+  private ?string $description = null;
 
-    #[ORM\ManyToOne(inversedBy: 'hostedOuting')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?User $host = null;
+  #[ORM\ManyToOne(inversedBy: 'hostedOuting')]
+  #[ORM\JoinColumn(nullable: false)]
+  private ?User $host = null;
 
-    /**
-     * @var Collection<int, User>
-     */
-    #[ORM\ManyToMany(targetEntity: User::class, mappedBy: 'enteredOuting')]
-    private Collection $attendees;
+  /**
+   * @var Collection<int, User>
+   */
+  #[ORM\ManyToMany(targetEntity: User::class, mappedBy: 'enteredOuting')]
+  private Collection $attendees;
 
-    #[ORM\ManyToOne(inversedBy: 'outing')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?Location $location = null;
+  #[ORM\ManyToOne(inversedBy: 'outing')]
+  #[ORM\JoinColumn(nullable: false)]
+  private ?Location $location = null;
 
-    #[ORM\ManyToOne(inversedBy: 'outings')]
-    #[ORM\JoinColumn(nullable: true)]
-    private ?Campus $campus = null;
+  #[ORM\ManyToOne(inversedBy: 'outings')]
+  #[ORM\JoinColumn(nullable: true)]
+  private ?Campus $campus = null;
 
-    #[ORM\ManyToOne(inversedBy: 'outings')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?Status $status = null;
+  #[ORM\ManyToOne(inversedBy: 'outings')]
+  #[ORM\JoinColumn(nullable: false)]
+  private ?Status $status = null;
 
-    public function __construct()
-    {
-        $this->attendees = new ArrayCollection();
+  public function __construct()
+  {
+    $this->attendees = new ArrayCollection();
+  }
+
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
+
+  public function getTitle(): ?string
+  {
+    return $this->title;
+  }
+
+  public function setTitle(string $title): static
+  {
+    $this->title = $title;
+
+    return $this;
+  }
+
+  public function getStartAt(): ?\DateTimeInterface
+  {
+    return $this->startAt;
+  }
+
+  public function setStartAt(?\DateTimeInterface $startAt): static
+  {
+    $this->startAt = $startAt;
+
+    return $this;
+  }
+
+  public function getDuration(): ?int
+  {
+    return $this->duration;
+  }
+
+  public function setDuration(?int $duration): static
+  {
+    $this->duration = $duration;
+
+    return $this;
+  }
+
+  public function getEndAt(): ?\DateTimeInterface
+  {
+    if ($this->startAt === null || $this->duration === null) {
+      return null;
+    }
+    return (clone $this->startAt)->modify(sprintf('+%d minutes', $this->duration));
+  }
+
+  public function getEntryDeadline(): ?\DateTimeInterface
+  {
+    return $this->entryDeadline;
+  }
+
+  public function setEntryDeadline(?\DateTimeInterface $entryDeadline): static
+  {
+    $this->entryDeadline = $entryDeadline;
+
+    return $this;
+  }
+
+  public function getMaxEntryCount(): ?int
+  {
+    return $this->maxEntryCount;
+  }
+
+  public function setMaxEntryCount(?int $maxEntryCount): static
+  {
+    $this->maxEntryCount = $maxEntryCount;
+
+    return $this;
+  }
+
+  public function getDescription(): ?string
+  {
+    return $this->description;
+  }
+
+  public function setDescription(?string $description): static
+  {
+    $this->description = $description;
+
+    return $this;
+  }
+
+  public function getHost(): ?User
+  {
+    return $this->host;
+  }
+
+  public function setHost(?User $host): static
+  {
+    $this->host = $host;
+
+    return $this;
+  }
+
+  /**
+   * @return Collection<int, User>
+   */
+  public function getAttendees(): Collection
+  {
+    return $this->attendees;
+  }
+
+  public function addAttendee(User $attendee): static
+  {
+    if (!$this->attendees->contains($attendee)) {
+      $this->attendees->add($attendee);
+      $attendee->addEnteredOuting($this);
     }
 
-    public function getId(): ?int
-    {
-        return $this->id;
+    return $this;
+  }
+
+  public function removeAttendee(User $attendee): static
+  {
+    if ($this->attendees->removeElement($attendee)) {
+      $attendee->removeEnteredOuting($this);
     }
 
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
+    return $this;
+  }
 
-    public function setTitle(string $title): static
-    {
-        $this->title = $title;
+  public function getLocation(): ?Location
+  {
+    return $this->location;
+  }
 
-        return $this;
-    }
+  public function setLocation(?Location $location): static
+  {
+    $this->location = $location;
 
-    public function getStartAt(): ?\DateTimeInterface
-    {
-        return $this->startAt;
-    }
+    return $this;
+  }
 
-    public function setStartAt(?\DateTimeInterface $startAt): static
-    {
-        $this->startAt = $startAt;
+  public function getCampus(): ?Campus
+  {
+    return $this->campus;
+  }
 
-        return $this;
-    }
+  public function setCampus(?Campus $campus): static
+  {
+    $this->campus = $campus;
 
-    public function getDuration(): ?int
-    {
-        return $this->duration;
-    }
+    return $this;
+  }
 
-    public function setDuration(?int $duration): static
-    {
-        $this->duration = $duration;
+  public function getStatus(): ?Status
+  {
+    return $this->status;
+  }
 
-        return $this;
-    }
+  public function setStatus(?Status $status): static
+  {
+    $this->status = $status;
 
-    public function getEndAt(): ?\DateTimeInterface
-    {
-        return $this->startAt?->modify(sprintf('+%d minutes', $this->duration));
-    }
-
-    public function getEntryDeadline(): ?\DateTimeInterface
-    {
-        return $this->entryDeadline;
-    }
-
-    public function setEntryDeadline(?\DateTimeInterface $entryDeadline): static
-    {
-        $this->entryDeadline = $entryDeadline;
-
-        return $this;
-    }
-
-    public function getMaxEntryCount(): ?int
-    {
-        return $this->maxEntryCount;
-    }
-
-    public function setMaxEntryCount(?int $maxEntryCount): static
-    {
-        $this->maxEntryCount = $maxEntryCount;
-
-        return $this;
-    }
-
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(?string $description): static
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
-    public function getHost(): ?User
-    {
-        return $this->host;
-    }
-
-    public function setHost(?User $host): static
-    {
-        $this->host = $host;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, User>
-     */
-    public function getAttendees(): Collection
-    {
-        return $this->attendees;
-    }
-
-    public function addAttendee(User $attendee): static
-    {
-        if (!$this->attendees->contains($attendee)) {
-            $this->attendees->add($attendee);
-            $attendee->addEnteredOuting($this);
-        }
-
-        return $this;
-    }
-
-    public function removeAttendee(User $attendee): static
-    {
-        if ($this->attendees->removeElement($attendee)) {
-            $attendee->removeEnteredOuting($this);
-        }
-
-        return $this;
-    }
-
-    public function getLocation(): ?Location
-    {
-        return $this->location;
-    }
-
-    public function setLocation(?Location $location): static
-    {
-        $this->location = $location;
-
-        return $this;
-    }
-
-    public function getCampus(): ?Campus
-    {
-        return $this->campus;
-    }
-
-    public function setCampus(?Campus $campus): static
-    {
-        $this->campus = $campus;
-
-        return $this;
-    }
-
-    public function getStatus(): ?Status
-    {
-        return $this->status;
-    }
-
-    public function setStatus(?Status $status): static
-    {
-        $this->status = $status;
-
-        return $this;
-    }
+    return $this;
+  }
 }
